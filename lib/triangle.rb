09460c5677f1d0844337f629f5739bc131d6095d
Anthony Pardo <pardo.anthony@gmail.com>
2020-01-25 20:00:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,9 @@ class Triangle
   def kind 
     if @one <= 0 or @two <= 0 or @three <= 0 
       raise TriangleError 
-    elsif @one + @two <= @three
+    elsif @one + @two <= @three or @one + @three <= @two or @two + @three <= @one 
+      raise TriangleError 
+    else 
   end
   
   class TriangleError < StandardError
