a7fbe024d8bf78c11ed6b13e54db4201a6c59146
Anthony Pardo <pardo.anthony@gmail.com>
2020-01-25 19:56:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,13 @@
 class Triangle
-  # write code here
+  attr_accessor :one, :two, :three 
+  
+  def initialize(one, two, three)
+    @one = one
+    @two = two 
+    @three = three
+  end
+  
+  def kind 
+    
+  end
 end
